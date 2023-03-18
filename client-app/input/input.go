c2e2a92c580ca6001d4d1b9cff4a506f0528d2b4
hhow09 <hhow09@gmail.com>
2023-03-19 02:56:30 +0800

    feat: input with donech

--- client-app/input/input.go
+++ client-app/input/input.go
@@ -18,48 +18,54 @@ type Input struct {
 	resultCh chan string
 }
 
-func NewInput(interrupt chan os.Signal, exitKeys []keyboard.Key) (*Input, chan string) {
+func NewInput(interrupt chan os.Signal, exitKeys []keyboard.Key) (*Input, chan string, chan interface{}) {
 	resultCh := make(chan string)
 	i := &Input{
 		resultCh: resultCh,
 	}
+	doneCh := make(chan interface{})
 
-	go i.readStdin(interrupt, exitKeys)
-	return i, resultCh
+	go i.readStdin(interrupt, exitKeys, doneCh)
+	return i, resultCh, doneCh
 }
 
-func (i *Input) readStdin(interrupt chan os.Signal, exitKeys []keyboard.Key) {
+func (i *Input) readStdin(interrupt chan os.Signal, exitKeys []keyboard.Key, doneCh chan interface{}) {
 	for {
-		char, key, err := keyboard.GetSingleKey()
-		if err != nil {
-			panic(err)
-		}
-
-		// exit conditions
-		for _, ekeys := range exitKeys {
-			if key == ekeys {
-				fmt.Println("exit")
-				interrupt <- os.Interrupt
-				return
-			}
-		}
-		switch key {
-		case keyboard.KeyEnter:
-			ClearLine()
-			i.resultCh <- strings.TrimSpace(string(i.buf))
-			i.resetBuffer()
-		case keyboard.KeyBackspace2, keyboard.KeyBackspace:
-			if len(i.buf) > 0 {
-				i.buf = i.buf[:len(i.buf)-1]
-			}
-		case keyboard.KeySpace:
-			i.buf = append(i.buf, rune(' '))
+		select {
+		case <-doneCh:
+			return
 		default:
-			i.buf = append(i.buf, char)
+			char, key, err := keyboard.GetSingleKey()
+			if err != nil {
+				panic(err)
+			}
+
+			// exit conditions
+			for _, ekeys := range exitKeys {
+				if key == ekeys {
+					fmt.Println("exit")
+					interrupt <- os.Interrupt
+					return
+				}
+			}
+			switch key {
+			case keyboard.KeyEnter:
+				ClearLine()
+				i.resultCh <- strings.TrimSpace(string(i.buf))
+				i.resetBuffer()
+			case keyboard.KeyBackspace2, keyboard.KeyBackspace:
+				if len(i.buf) > 0 {
+					i.buf = i.buf[:len(i.buf)-1]
+				}
+			case keyboard.KeySpace:
+				i.buf = append(i.buf, rune(' '))
+			default:
+				i.buf = append(i.buf, char)
+			}
+			// print out
+			ClearLine()
+			fmt.Printf("%v", string(i.buf))
 		}
-		// print out
-		ClearLine()
-		fmt.Printf("%v", string(i.buf))
 	}
 }
 
